6ff7fe453aaff78c9b3d35a9723a04206e9a8ddc
Arkadiy <woodythps@gmail.com>
2023-05-28 18:42:06 +0300

    add consts

---
+++ src/constants/constants.ts
@@ -0,0 +1,3 @@
+export const API_URL = "https://api.kinopoisk.dev";
+export const API_KEY = "A1FV80V-DEC4AS6-MH54J4V-5S42456";
+export const text = 'По вашему запросу ничего не найдено. Проверьте корректность введенных данных.';
