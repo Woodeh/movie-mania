5ecdcf19f3ad3024b96a031405978fdf8c778340
Arkadiy <woodythps@gmail.com>
2023-06-30 22:52:50 +0300

    upd

--- src/components/ShareButton/ShareButton.tsx
+++ src/components/ShareButton/ShareButton.tsx
@@ -1,51 +1,51 @@
-import React from 'react';
-import { TelegramShareButton, WhatsappShareButton, FacebookShareButton } from 'react-share';
-import { TelegramIcon, WhatsappIcon, FacebookIcon } from 'react-share';
+import React from "react";
+import {
+  TelegramShareButton,
+  WhatsappShareButton,
+  FacebookShareButton,
+} from "react-share";
+import { TelegramIcon, WhatsappIcon, FacebookIcon } from "react-share";
 
 export const ShareButton = () => {
-    const shareUrl = window.location.href;
-    const title = 'Movie Mania';
-  
-    const iconSize = 32;
-    const isRoundIcon = false;
-    const borderRadius = 10;
-    const bgStyle = { fill: '#430e8a' };
-    const iconFillColor = 'white';
-  
-    return (
-      <div className="share-buttons">
-        <TelegramShareButton url={shareUrl}>
-          <TelegramIcon
-            size={iconSize}
-            round={isRoundIcon}
-            borderRadius={borderRadius}
-            bgStyle={bgStyle}
-            iconFillColor={iconFillColor}
-          />
-          
-        </TelegramShareButton>
-  
-        <WhatsappShareButton url={shareUrl} title={title}>
-          <WhatsappIcon
-            size={iconSize}
-            round={isRoundIcon}
-            borderRadius={borderRadius}
-            bgStyle={bgStyle}
-            iconFillColor={iconFillColor}
-          />
-          
-        </WhatsappShareButton>
+  const shareUrl = window.location.href;
+  const title = "Movie Mania";
+  const iconSize = 32;
+  const isRoundIcon = false;
+  const borderRadius = 10;
+  const bgStyle = { fill: "#430e8a" };
+  const iconFillColor = "white";
 
-        <FacebookShareButton url={shareUrl} title={title}>
-          <FacebookIcon
-            size={iconSize}
-            round={isRoundIcon}
-            borderRadius={borderRadius}
-            bgStyle={bgStyle}
-            iconFillColor={iconFillColor}
-          />
-          
-        </FacebookShareButton>
-      </div>
-    );
-  };
+  return (
+    <div className="share-buttons">
+      <TelegramShareButton url={shareUrl}>
+        <TelegramIcon
+          size={iconSize}
+          round={isRoundIcon}
+          borderRadius={borderRadius}
+          bgStyle={bgStyle}
+          iconFillColor={iconFillColor}
+        />
+      </TelegramShareButton>
+
+      <WhatsappShareButton url={shareUrl} title={title}>
+        <WhatsappIcon
+          size={iconSize}
+          round={isRoundIcon}
+          borderRadius={borderRadius}
+          bgStyle={bgStyle}
+          iconFillColor={iconFillColor}
+        />
+      </WhatsappShareButton>
+
+      <FacebookShareButton url={shareUrl} title={title}>
+        <FacebookIcon
+          size={iconSize}
+          round={isRoundIcon}
+          borderRadius={borderRadius}
+          bgStyle={bgStyle}
+          iconFillColor={iconFillColor}
+        />
+      </FacebookShareButton>
+    </div>
+  );
+};
