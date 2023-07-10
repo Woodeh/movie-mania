50b1472236d6ca3dc20a19f8e8728ccd5186d92b
Arkadiy <woodythps@gmail.com>
2023-07-10 13:23:35 +0300

    Update urls.ts

--- src/utils/api/urls.ts
+++ src/utils/api/urls.ts
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://studapi.teachmeskills.by';
-export const FILM_URL = 'http://www.omdbapi.com/';
+export const FILM_URL = 'https://www.omdbapi.com/';
 
 export const urls = {
     GET_POSTS: `${BASE_URL}/blog/posts?lesson_num=2023&limit=20&offset=20`,
